d8e77ecb46c0c218827f6403d3f649f444596745
Rafael Ferreira Silva <rafael@rafaelsilva.net>
2011-11-10 16:04:25 -0200

    Improved drupal sites search

--- checkup.py
+++ checkup.py
@@ -3,8 +3,7 @@
 
 # Author Rafael Ferreira Silva - http://rafaelsilva.net
 # See LICENSE.txt for details.
-
-import os
+import os, fnmatch
 
 class Updater:
 
@@ -12,23 +11,21 @@ class Updater:
     self.path = path
     self.paths = []
     if (os.path.exists(path)):
-      for file_dir in os.listdir(path):
-        full_path =  path + "/" + file_dir
-        if os.path.isdir(full_path) and file_dir[0] != ".":
-          self.paths.append(full_path)
+      for root, dirnames, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, 'settings.php'):
+          droot, fname = os.path.split(os.path.split(root)[0])
+          self.paths.append(os.path.join(droot))
 
   def check(self):
     for path in self.paths:
-      if os.path.isfile(path + "/cron.php"):
-        print "\033[1;33mDrupal site: "+ path+"\033[0m"
-        os.system("drush up -r "+path)
-        os.system("sleep 5")
-        os.system("clear")
-    
+      print "\033[1;33mDrupal site: "+ path+"\033[0m"
+      os.system("drush up -y -r "+path)
+      os.system("sleep 5")
+      os.system("clear")
+
 
 #main
 if __name__ == "__main__":
   #change this path to the one your sites live
   up = Updater('/var/www')
   up.check()
-
