5350b4e0bf820c5f36f845e31bad7c39bc2a248d
Rafael Ferreira Silva <rafael@rafaelsilva.net>
2012-03-30 10:29:32 -0300

    Code modified to check and update multisites (now working)

--- checkup.py
+++ checkup.py
@@ -10,23 +10,28 @@ class Updater:
   def __init__(self, path = "./"):
     self.path = path
     self.paths = []
-    if (os.path.exists(path)):                                                                      
-      for root, dirnames, filenames in os.walk(path):                                               
-        for filename in fnmatch.filter(filenames, 'settings.php'):                                  
-          droot, fname = os.path.split(root)                                                        
-          if fname == "default":                                                                    
-            droot, fname = os.path.split(droot)                                                     
-            self.paths.append([droot, None])                                                        
-          else:                                                                                     
-            self.paths.append([os.path.join(droot, fname), fname])                                  
-                                                                                                    
-  def check(self):                                                                                  
-    for path in self.paths:                                                                         
-      print "\033[1;33mDrupal site: "+ path[0]+"\033[0m"                                            
-      if path[1]:                                                                                  
-        os.system("drush up -r "+path[0]+" -l "+path[1])                          
-      else:                                                                                         
-        os.system("drush up -r "+path[0]) 
+    if (os.path.exists(path)):
+      for root, dirnames, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, 'settings.php'):
+          droot, fname = os.path.split(root)
+          # checking if it's a default or a multisite
+          if fname == "default":
+            droot, fname = os.path.split(droot)
+            self.paths.append([droot, None])
+          else:
+            droot, fname = os.path.split(droot)
+            self.paths.append([droot, fname])
+
+  def check(self):
+    for path in self.paths:
+      print "\033[1;33mDrupal site: "+ path[0]+"\033[0m"
+      # multisite update
+      if path[1]:
+        os.system("drush up -r "+path[0]+" -l "+path[1])
+        print "drush up -r "+path[0]+" -l "+path[1]
+      # normal update
+      else:
+        os.system("drush up -r "+path[0])
       os.system("sleep 5")
       os.system("clear")
 
