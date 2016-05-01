dea8cd2e699b1f8c81e24c5b20b8abc60e483797
Rafael Silva <rafael@rafaelsilva.net>
2016-05-01 12:20:42 -0300

    Update checkup.py
    updated to check drupal 8 sites and use correct cache clear command

--- checkup.py
+++ checkup.py
@@ -3,7 +3,7 @@
 
 # Author Rafael Ferreira Silva - http://rafaelsilva.net
 # See LICENSE.txt for details.
-import os, fnmatch
+import os, fnmatch, subprocess
 
 class Updater:
 
@@ -14,29 +14,36 @@ class Updater:
       for root, dirnames, filenames in os.walk(path):
         for filename in fnmatch.filter(filenames, 'settings.php'):
           droot, fname = os.path.split(root)
-          # checking if it's a default or a multisite
           if fname == "default":
             droot, fname = os.path.split(droot)
             self.paths.append([droot, None])
           else:
             droot, fname = os.path.split(droot)
             self.paths.append([droot, fname])
-    self.paths = sorted(self.paths)
 
   def check(self):
     for path in self.paths:
+      #if Drupal 8, use drush cache-rebuild instead of cache clear
+      version = subprocess.Popen("drush -r "+path[0]+" php-eval 'echo VERSION'", shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()[0]
+      if (version == "VERSION")
+        cache_clear_command = "/root/tools/drush/drush cr"
+      else
+        cache_clear_command = "/root/tools/drush/drush cc all"
       print "\033[1;33mDrupal site: "+ path[0]+"\033[0m"
-      # multisite update
       if path[1]:
-        os.system("drush up -r "+path[0]+" -l "+path[1])
-      # normal update
+        subprocess.call(cache_clear_command+" -r "+path[0]+" -l "+path[1], shell=True)
+        subprocess.call("/root/tools/drush/drush vset update_last_check 0 -r "+path[0]+" -l "+path[1], shell=True)
+        subprocess.call("/root/tools/drush/drush cron -y -r "+path[0]+" -l "+path[1], shell=True)
+        subprocess.call("/root/tools/drush/drush up -y -r "+path[0]+" -l "+path[1], shell=True)
       else:
-        os.system("drush up -r "+path[0])
-      os.system("sleep 5")
-      os.system("clear")
+        subprocess.call(cache_clear_command+" -r "+path[0], shell=True)
+        subprocess.call("/root/tools/drush/drush vset update_last_check 0 -r "+path[0] , shell=True)
+        subprocess.call("/root/tools/drush/drush cron -y -r "+path[0], shell=True)
+        subprocess.call("/root/tools/drush/drush up -y -r "+path[0], shell=True)
+
 
 #main
 if __name__ == "__main__":
   #change this path to the one your sites live
-  up = Updater('/var/www')
+  up = Updater('/home')
   up.check()
